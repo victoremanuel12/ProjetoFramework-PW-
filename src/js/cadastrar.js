ce53066c8082ef59e9af6c64052bad987cdb46ba
Victor Emanuel <victoremannuel12@hotmail.com>
2022-12-12 14:43:35 -0300

    Final do prototipo apra apresentação

--- src/js/cadastrar.js
+++ src/js/cadastrar.js
@@ -1,5 +1,5 @@
-
-function ValidarSenha(event) {
+function ValidarCadastro(event) {
+   let CadastrarUsuario = false
    event.preventDefault();
    const errorSubmitDados = document.querySelector('.errorSubmitDados')
    const inputs = document.querySelectorAll('.inputDados');
@@ -7,37 +7,41 @@ function ValidarSenha(event) {
    const confirmarSenha = document.querySelector('#confirmarSenha').value.trim();
    let empty = false;
    inputs.forEach(input => {
-      if (input.value == "") empty = true; 
+      if (input.value == "") empty = true;
    })
    let arraySenha = senha.split('')
    let senhaComNumero = false;
    let senhaComLetra = false;
    arraySenha.forEach(item => {
-      if (!isNaN(item)  ) senhaComNumero = true;
-      if(isNaN(item)) senhaComLetra= true;
+      if (!isNaN(item)) senhaComNumero = true;
+      if (isNaN(item)) senhaComLetra = true;
    })
    if (empty) {
       errorSubmitDados.classList.remove('alertSucess')
       errorSubmitDados.classList.add('alertImputEmpty')
-      errorSubmitDados.textContent="Preencha todos os Campos!"
-   }else if (senha != confirmarSenha) {
+      errorSubmitDados.textContent = "Preencha todos os Campos!"
+   } else if (senha != confirmarSenha) {
       errorSubmitDados.classList.remove('alertSucess')
       errorSubmitDados.classList.add('alertImputEmpty')
-      errorSubmitDados.textContent ="Senhas não Conferem!"
-   } else if(arraySenha.length  < 5){
+      errorSubmitDados.textContent = "Senhas não Conferem!"
+   } else if (arraySenha.length < 5) {
       errorSubmitDados.classList.remove('alertSucess')
       errorSubmitDados.classList.add('alertImputEmpty')
-      errorSubmitDados.textContent ="A senha deve conter no minimo 5 caracters!"
-   }else if( senhaComNumero == false || senhaComLetra == false){
+      errorSubmitDados.textContent = "A senha deve conter no minimo 5 caracters!"
+   } else if (senhaComNumero == false || senhaComLetra == false) {
       errorSubmitDados.classList.remove('alertSucess')
       errorSubmitDados.classList.add('alertImputEmpty')
-      errorSubmitDados.textContent="A senha tem que ter ao menos 1 número e 1 letra!"
-   }else if(senha == confirmarSenha && empty == false && senhaComNumero && senhaComLetra){
+      errorSubmitDados.textContent = "A senha tem que ter ao menos 1 número e 1 letra!"
+   } else if (senha == confirmarSenha && empty == false && senhaComNumero && senhaComLetra) {
+      CadastrarUsuario = true
       errorSubmitDados.classList.add('alertSucess')
-      errorSubmitDados.textContent="Usuario cadastrado com sucesso!"
+      errorSubmitDados.textContent = "Usuario cadastrado com sucesso!"
    }
-  
+   module.exports = CadastrarUsuario
 }
+
+
+
 
 
 
